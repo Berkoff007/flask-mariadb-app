3cd63cc8150c78b4326776006aaeac2399d9fe49
Berkoff007 <adamouedmond7@gmail.com>
2025-05-10 14:16:17 +0200

    Mises à jour des fichiers config

--- application/app/config.py
+++ application/app/config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    DB_USER = os.getenv("MYSQLUSER", "root")
-    DB_PASSWORD = os.getenv("MYSQLPASSWORD", "root")
-    DB_HOST = os.getenv("MYSQLHOST", "localhost")
+    DB_USER = os.getenv("MYSQLUSER")
+    DB_PASSWORD = os.getenv("MYSQLPASSWORD")
+    DB_HOST = os.getenv("MYSQLHOST")
     DB_PORT = os.getenv("MYSQLPORT", "3306")
-    DB_NAME = os.getenv("MYSQLDATABASE", "annuaires_student")
+    DB_NAME = os.getenv("MYSQLDATABASE")  # <-- ici on prend 'railway'
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
