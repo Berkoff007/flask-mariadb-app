3a0748ddf6bcacb2b870a4cbf94a4d39a9b58c06
Berkoff007 <adamouedmond7@gmail.com>
2025-05-10 08:16:20 +0200

    Mise à jour des fichiers config et authentification

--- application/app/config.py
+++ application/app/config.py
@@ -1,15 +1,16 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev")
 
-    DB_USER = os.getenv('MARIADB_USER', 'root')
-    DB_PASSWORD = os.getenv('MARIADB_PASSWORD', 'root')
-    DB_NAME = os.getenv('MARIADB_DATABASE', 'annuaires_student')
-    DB_HOST = os.getenv('MARIADB_HOST', 'db')  # "db" = nom du service Docker
-    DB_PORT = os.getenv('MARIADB_PORT', '3306')  # Port explicite
+    DB_USER = os.getenv("MYSQLUSER")
+    DB_PASSWORD = os.getenv("MYSQLPASSWORD")
+    DB_NAME = os.getenv("MYSQLDATABASE")
+    DB_HOST = os.getenv("MYSQLHOST")
+    DB_PORT = os.getenv("MYSQLPORT", "3306")
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
